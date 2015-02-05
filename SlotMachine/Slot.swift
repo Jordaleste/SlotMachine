3aec1680d0ae5ad096d8624a0fac85907b1539c4
Jordaleste <charlie@esnenterprises.com>
2015-02-04 19:48:46 -0500

    End of course

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Charles Eison on 2/1/15.
+//  Copyright (c) 2015 Charles Eison. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
